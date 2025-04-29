d66a6935aa20a1173960d336bac6fa03ebc4fcc5
hauhoa123r <hoahvhe186810@fpt.edu.vn>
2025-04-29 14:11:22 +0700

    clean architecture add factory

--- src/java/user/factory/NewClass.java
+++ src/java/user/factory/NewClass.java
@@ -2,8 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package myexception;
+package user.factory;
 
+/**
+ *
+ * @author ngoct
+ */
 public class NewClass {
     
 }
