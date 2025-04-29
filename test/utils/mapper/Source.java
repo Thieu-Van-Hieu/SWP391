6a139ea96187480c79c9d06a95b2ace090d8ae08
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-04-29 17:07:20 +0700

    Create Unit Test for Mapper

---
+++ test/utils/mapper/Source.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package utils.mapper;
+
+public class Source {
+	private int x;
+	private String y;
+	private double z;
+
+	public Source() {
+	}
+
+	public Source(int x, String y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
