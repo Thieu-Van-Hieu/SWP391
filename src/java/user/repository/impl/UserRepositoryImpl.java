69390d963111bf57f3e791e5c672e738ef39d04c
hauhoa123r <hoahvhe186810@fpt.edu.vn>
2025-04-30 17:32:12 +0700

    conflic with rain

---
+++ src/java/user/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package user.repository.impl;
+
+import config.DBContext;
+import java.sql.*;
+import user.dto.login.LoginRequestDTO;
+import user.entity.UserEntity;
+import user.repository.itf.UserRepository;
+
+/**
+ *
+ * @author ngoct
+ */
+public class UserRepositoryImpl implements UserRepository {
+
+    @Override
+    public UserEntity isLogin(LoginRequestDTO loginRequestDTO) {
+        DBContext db = DBContext.getInstance();
+        UserEntity result = new UserEntity();
+        try {
+            String sql = """
+                         select * from users 
+                         where username = ? and password = ?
+                         """;
+            PreparedStatement st = db.getConnection().prepareStatement(sql);
+            st.setString(1, loginRequestDTO.getUsername());
+            st.setString(2, loginRequestDTO.getPassword());
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                result.setUserName(rs.getString("username"));
+                result.setId(rs.getInt("id"));
+                result.setRole(rs.getInt("role"));
+            }
+        } catch (Exception e) {
+            return new UserEntity();
+        }
+        return result;
+    }
+}
