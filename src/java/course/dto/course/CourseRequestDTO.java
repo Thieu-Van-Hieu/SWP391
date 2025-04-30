23491b29055525cbcd2853d9bb3199c33d3912dd
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-04-30 19:58:52 +0700

    Create course request dto

---
+++ src/java/course/dto/course/CourseRequestDTO.java
@@ -0,0 +1,25 @@
+package course.dto.course;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class CourseRequestDTO {
+	private int id;
+
+	public CourseRequestDTO() {
+	}
+
+	public CourseRequestDTO(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
