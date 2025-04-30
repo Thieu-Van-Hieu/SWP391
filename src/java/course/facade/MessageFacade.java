d5eab680862357e2ae7f9ae4d1b2a459bdd3e6e7
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-04-30 19:57:54 +0700

    Create message facade

---
+++ src/java/course/facade/MessageFacade.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package course.facade;
+
+import course.converter.course.CourseRequestConverter;
+import course.converter.message.MessageRequestConverter;
+import course.dto.course.CourseRequestDTO;
+import course.dto.message.MessageRequestDTO;
+import course.dto.message.MessageResponseDTO;
+import course.factory.service.MessageServiceFactory;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+
+public class MessageFacade {
+
+	public static boolean sendMessage(HttpServletRequest request) {
+		MessageRequestDTO messageRequestDTO = MessageRequestConverter.toConverterMessage(request);
+		return MessageServiceFactory.getMessageService().sendMessage(messageRequestDTO);
+	}
+
+	public static boolean editMessage(HttpServletRequest request) {
+		MessageRequestDTO messageRequestDTO = MessageRequestConverter.toConverterMessage(request);
+		return MessageServiceFactory.getMessageService().editMessage(messageRequestDTO);
+	}
+
+	public static boolean deleteMessage(HttpServletRequest request) {
+		MessageRequestDTO messageRequestDTO = MessageRequestConverter.toConverterMessage(request);
+		return MessageServiceFactory.getMessageService().deleteMessage(messageRequestDTO);
+	}
+
+	public static ArrayList<MessageResponseDTO> getMessagesByCourseId(HttpServletRequest request) {
+		CourseRequestDTO courseRequestDTO = CourseRequestConverter.toConverterCourse(request);
+		return MessageServiceFactory.getMessageService().getMessagesByCourseId(courseRequestDTO);
+	}
+}
