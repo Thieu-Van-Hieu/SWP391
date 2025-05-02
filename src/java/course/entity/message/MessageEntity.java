63cda00c47dac3915839eb215626ee6b9a059e89
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-05-02 09:06:09 +0700

    Use lombok

--- src/java/course/entity/message/MessageEntity.java
+++ src/java/course/entity/message/MessageEntity.java
@@ -5,63 +5,20 @@
 package course.entity.message;
 
 import java.sql.Timestamp;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageEntity {
 
-    private int messageId;
-    private int courseId;
-    private int memberId;
-    private String content;
-    private Timestamp sendAt;
-
-	public MessageEntity() {
-	}
-
-	public MessageEntity(int messageId, int courseId, int memberId, String content, Timestamp sendAt) {
-		this.messageId = messageId;
-		this.courseId = courseId;
-		this.memberId = memberId;
-		this.content = content;
-		this.sendAt = sendAt;
-	}
-
-	public int getMessageId() {
-		return messageId;
-	}
-
-	public void setMessageId(int messageId) {
-		this.messageId = messageId;
-	}
-
-	public int getCourseId() {
-		return courseId;
-	}
-
-	public void setCourseId(int courseId) {
-		this.courseId = courseId;
-	}
-
-	public int getMemberId() {
-		return memberId;
-	}
-
-	public void setMemberId(int memberId) {
-		this.memberId = memberId;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public Timestamp getSendAt() {
-		return sendAt;
-	}
-
-	public void setSendAt(Timestamp sendAt) {
-		this.sendAt = sendAt;
-	}
+	private int messageId;
+	private int courseId;
+	private int memberId;
+	private String content;
+	private Timestamp sendAt;
 }
