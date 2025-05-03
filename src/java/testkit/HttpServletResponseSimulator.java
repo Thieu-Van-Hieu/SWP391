76f3a30955c4a8931854015697144c53e97a3fe3
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-05-03 11:58:01 +0700

    Create class for simulating response

---
+++ src/java/testkit/HttpServletResponseSimulator.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package testkit;
+
+import jakarta.servlet.http.HttpServletResponse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doNothing;
+
+public class HttpServletResponseSimulator {
+	
+	private HttpServletResponse response;
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+	
+	public static Builder builder() {
+		return new Builder();
+	}
+	
+	public static class Builder {
+		private HttpServletResponse response = mock(HttpServletResponse.class);
+		
+		public HttpServletResponseSimulator build() {
+			return new HttpServletResponseSimulator(response);
+		}
+	}
+	
+	private void disableSendRedirect() throws Exception {
+		doNothing().when(response).sendRedirect(anyString());
+	}
+	
+	private HttpServletResponseSimulator(HttpServletResponse response) {
+		this.response = response;
+		
+		try {
+			disableSendRedirect();
+		} catch (Exception e) {
+		}
+	}
+}
