06dee5a837f486407a91ab1bd39f2f919fc4ac6d
Mr.NoBody <hieutvhe186966@fpt.edu.vn>
2025-05-04 17:13:48 +0700

    Override equals for MessageEntity to accept small value differences

--- src/java/course/entity/MessageEntity.java
+++ src/java/course/entity/MessageEntity.java
@@ -1,8 +1,8 @@
 package course.entity;
 
 import java.sql.Timestamp;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,7 +13,6 @@ import utils.tablemapper.annotation.Table;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 @Table(name = "messages")
 public class MessageEntity {
         
@@ -23,4 +22,40 @@ public class MessageEntity {
 	private int memberId;
 	private String content;
 	private Timestamp sendAt;
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MessageEntity other = (MessageEntity) obj;
+        if (this.messageId != other.messageId) {
+            return false;
+        }
+        if (this.courseId != other.courseId) {
+            return false;
+        }
+        if (this.memberId != other.memberId) {
+            return false;
+        }
+        if (!Objects.equals(this.content, other.content)) {
+            return false;
+        }
+        
+        final int DELTA = 2;
+        final long diff = Math.abs(this.sendAt.getTime() - other.sendAt.getTime());
+        return diff <= DELTA;
+    }
 }
